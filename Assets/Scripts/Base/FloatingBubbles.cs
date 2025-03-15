1de9e4af2fe4f9577cea4e3514fea473cf9e4777
Joseajierro <jrmar0805@gmail.com>
2025-03-15 18:02:24 +0100

    musica

--- Assets/Scripts/Base/FloatingBubbles.cs
+++ Assets/Scripts/Base/FloatingBubbles.cs
@@ -9,11 +9,14 @@ namespace Base
         [SerializeField] private float floatSpeed = 1.0f;    // Velocidad del movimiento
         [SerializeField] private float lateralMovementRange = 18f; // Rango de movimiento lateral (X)
         [SerializeField] private float lateralSpeed = 3.5f; // Velocidad lateral
-
+        
+        
+        private AudioSource bubbleSound;
         private Vector3 startPos;
 
         private void Start()
         {
+            bubbleSound = GetComponent<AudioSource>();
             startPos = transform.position; // Guardamos la posición inicial de la burbuja
         }
 
@@ -27,6 +30,11 @@ namespace Base
 
             // Aplicamos el nuevo movimiento a la posición
             transform.position = new Vector3(startPos.x + newX, startPos.y + newY, startPos.z);
+        }
+
+        public void PlayBubbleSound()
+        {
+            bubbleSound.PlayOneShot(bubbleSound.clip);
         }
     }
 }
